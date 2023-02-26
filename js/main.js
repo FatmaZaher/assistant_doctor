d17a9358117ef387d32da3763f981923bfb1d249
FatmaZaher <fatmaabdulhameid@gmail.com>
2023-02-26 18:00:59 +0200

    add comment

--- js/main.js
+++ js/main.js
@@ -1,12 +1,8 @@
 $(document).ready(function () {
-	/*====================================
-		Nice Select JS
-	======================================*/
+	// Nice Select JS
 	$('select').niceSelect();
 
-	/*=====================================
-		Date Picker JS
-	======================================*/
+	// Date Picker JS
 	$(function () {
 		$("#datepicker").datepicker();
 	});
@@ -18,6 +14,27 @@ $(document).ready(function () {
 		var fileName = $(this).val().split('\\').pop();
 		$(this).siblings('label').text(fileName);
 	});
+
+	// Get the input element and the image element
+	const inputElement = $("#file-upload");
+	const imageElement = $("#profile-image");
+
+	// When a new image is selected, update the image element with the new image
+	inputElement.on("change", function () {
+		const file = this.files[0];
+		const reader = new FileReader();
+		reader.readAsDataURL(file);
+		reader.onload = function () {
+			const dataURL = reader.result;
+			imageElement.attr("src", dataURL);
+		};
+	});
+
+	// When the upload button is clicked, trigger the input element to open the file selection dialog
+	$("#upload-button").on("click", function () {
+		inputElement.click();
+	});
+
 });
 
 /*====================
